f3e90af1054e83c5ad4976c8df23c7bfd35532af
ReedBelca <reedbelca55@gmail.com>
2026-04-21 10:08:23 +0000

    Customization

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="bg-green-600 text-white">Hello world!</h1>
-      <h1 className="border-4 border-blue-500 text-vuejs-100">Hello world!</h1>
-    </div>
+    <main>
+      {/* Colors */}
+      <div>
+        <h1 className="bg-green-600 text-white">Hello world!</h1>
+        <h1 className="border-4 border-blue-500 text-vuejs-100">Hello world!</h1>
+      </div>
+
+      {/* Customization */}
+      <div>
+        <h1 className="text-xs sm:text-base md:text-lg lg:text-3xl">Hello long sentence</h1>
+        <h1 className="mb-4">Hello Spacing</h1>
+      </div>
+    </main>
   );
 }
