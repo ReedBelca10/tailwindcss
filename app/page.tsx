a987ce8d8fe1fd575a759b520ee83975400aa154
ReedBelca <reedbelca55@gmail.com>
2026-04-21 09:09:45 +0000

    Colors

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <h1 className="bg-green-600 text-white">Hello world!</h1>
+      <h1 className="border-4 border-blue-500 text-vuejs-100">Hello world!</h1>
+    </div>
   );
 }
