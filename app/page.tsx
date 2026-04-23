0127b300694a2dc341ce07f8fcef11f42fed430f
ReedBelca <reedbelca55@gmail.com>
2026-04-23 10:04:58 +0000

    Typography

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,16 @@ export default function Home() {
         <h1 className="text-xs sm:text-base md:text-lg lg:text-3xl">Hello long sentence</h1>
         <h1 className="mb-4">Hello Spacing</h1>
       </div>
+
+      {/* Typography */}
+      <div>
+        <h1 className="text-3xl">Titre 1</h1>
+        <h2 className="text-2xl">Titre 2</h2>
+        <h3 className="text-xl">Titre 3</h3>
+        <p className="text-base italic underline decoration-red-500 decoration-double underline-offset-4">A regular paragraph</p>
+        <p className="text-sm truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p className="text-xs note">A little note</p>
+      </div>
     </main>
   );
 }
