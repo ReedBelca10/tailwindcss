ff9be847c0766b7cfc0747940b2831e6981d70a9
ReedBelca <reedbelca55@gmail.com>
2026-04-30 00:57:20 +0000

    Animations & Transitio

--- app/page.tsx
+++ app/page.tsx
@@ -109,6 +109,45 @@ export default function Home() {
           </button>
         </div>
       </div>
+      <hr />
+
+      {/* System Design */}
+      <div>
+        <h1>This is a title</h1>
+        <h2>This is a subtitle</h2>
+        <p className="text-base">This is a regular paragraph</p>
+        <a href="#">This is an ancor</a>
+        <div className="my-4">
+          <button className="btn btn-primary">This is a primary button</button>
+        </div>
+        <div className="my-4">
+          <button className="btn btn-secondary">This is a secondary button</button>
+        </div>
+        <div>
+          <input type="text" placeholder="This is an input"/>
+        </div>
+        <div>
+          <input disabled type="text" placeholder="This is a disabled input"/>
+        </div>
+        <div>
+          <input type="date"/>
+        </div>
+        <div className="flex items-start my-4">
+          <input type="checkbox" id="checkbox"/>
+          <label htmlFor="checkbox">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque? Lorelum ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque?
+          </label>
+        </div>
+        <div>
+          <select>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+        </div>
+        <h1>This is another title</h1>
+        <h2>This is another subtitle</h2>
+      </div>
     </main>
   );
 }
